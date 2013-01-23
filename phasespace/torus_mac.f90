8dc46f630a55081cc9d940606ed9165d04ba5fea
Carlos Pineda <carlospgmat03@gmail.com>
2013-01-23 15:47:07 -0600

    funciona si se pasa la funcion resumida al principal. no tiene modulos

--- phasespace/torus_mac.f90
+++ phasespace/torus_mac.f90
@@ -1,3 +1,28 @@
+subroutine wigner_from_state(phi,wigner) !{{{
+  implicit none
+  complex(kind(1d0)), intent(in)   :: phi(0:)
+  complex(kind(1d0)), allocatable  :: wigner_tmp(:,:)
+  real(kind(1d0)), intent(out)     :: wigner(:,:)
+  complex(kind(1d0)), allocatable  :: rho1(:,:), work1(:)
+  integer n, nr
+  print*,"En la rutina, ", size(phi)
+  n=size(phi)
+  if ((size(wigner,1).ne.2*n).or.(size(wigner,2).ne.2*n)) then
+    print*,"Tamanos no compatibles en la rutina wigner_from_state"
+    print*,size(phi), size(wigner,1), size(wigner,2)
+  endif
+  allocate (rho1(n,n), work1(4*n+16), wigner_tmp(2*n,2*n))
+  wigner_tmp=0d0
+  call state2kirk(n,phi,0d0,0d0,rho1,n,work1)
+  call kirk2wig(1,n,rho1,n,nr,wigner_tmp,2*n,work1)
+  wigner_tmp=wigner_tmp/(2*n)
+  call zfft2d(1,2*n,2*n,wigner_tmp,2*n,work1)
+  call zfft2d(-1,2*n,2*n,wigner_tmp,2*n,work1)
+  wigner_tmp=wigner_tmp/(4*n*n)
+  deallocate(rho1, work1)
+  wigner=real(wigner_tmp)
+  deallocate(wigner_tmp)
+end subroutine wigner_from_state ! }}}
 subroutine state2kirk(n,phi,tip,tiq,rho,ndim,workfft) ! {{{
 !  
 ! constructs the kirkwood representation of the pure state vector phi
--- phasespace/wigner.f90
+++ phasespace/wigner.f90
@@ -2,35 +2,14 @@ Program semiagam
 ! Constantes y definicion de variables  {{{
   parameter(nmax=510)     ! maximum dimension of Hilbert space 500
   parameter(ndist=2*nmax)    ! maximum dimension of distributions
-  real*8 pr1,pr2,tip,tiq,dpr1,dpr2,rnum1,rnum2,beta,hpr1,hpr2
-  real*4 spl(1:2*nmax,1:2*nmax)
-  complex*16 sigma,phi(0:nmax-1),phi2(0:nmax-1),phi3(0:nmax-1)
-  complex*16 rho1(0:nmax-1,0:nmax-1),rho2(0:nmax-1,0:nmax-1)
-  complex*16 rho3(0:nmax-1,0:nmax-1)
-  complex*16 wigner(0:ndist-1,0:ndist-1),cmunu(0:ndist-1,0:ndist-1),temp
-  complex*16 cmn(0:ndist-1,0:ndist-1)
-  complex*16 hus(0:ndist-1,0:ndist-1),hus2(0:ndist-1,0:ndist-1)
-  complex*16 chord(0:ndist-1,0:ndist-1)
-  equivalence (wigner(0,0),hus(0,0))!,chord(0,0))
-  complex*16 work1(2*ndist+16),work2(2*ndist+16),work3(2*ndist+16)
-  complex*16 psptrace,norm1,norm2,d1,d2
-  complex*16 overlap,echo,entro2,echo1,echo2
-  real*8 slope,entro1,q,p,q2,p2,pi,dospi
-  real*8 entropy,epsp,epsq,entr1,entr2
-  character*3 type1,type2,map,rep,noise,cene,ckmax
-  character*5 col,cpr1,cepsp,cpi,cqi
-  character*30 title,sub,ty
-  character*25 fnombre,aname
-  character*1 test2,dummy3,evtype,ans1
-  character*2 stateop,var1
-  character*6 reptype
-  Real tr(6)
-  integer direction,fsteps
-  Integer,Parameter::ndim=16000
-  real(8) xx(ndim),yy(ndim),xe(ndim),ye(ndim)
+  complex(kind(1d0)) ::  phi(0:nmax-1)
+  complex(kind(1d0)), allocatable ::  phi_a(:)
+  complex*16 rho1(0:nmax-1,0:nmax-1)
+  complex*16 wigner(0:ndist-1,0:ndist-1)
+  real(kind(1d0)), allocatable :: wigner_tst(:,:)
+  complex*16 work1(2*ndist+16)
 
   pi=4.d0*datan(1.d0)
-  tip=0.d0;tiq=0.d0
   ! }}}
 ! Read the state {{{
   n=0
@@ -42,24 +21,68 @@ Program semiagam
   end do
 10 continue
 ! }}}
-! PRocesamiento de la informacion {{{
+ allocate(phi_a(n), wigner_tst(2*n,2*n))
+ phi_a=phi(0:n-1)
+!  print*, "hola"
+!  call wigner_from_state_2(phi_a,wigner_tst)
+ print*, size(phi_a)
+ call wigner_from_state(phi_a,wigner_tst)
+!  print*, "adios"
+! Procesamiento de la informacion {{{
   !call init_state(n,nr,q,p,tip,tiq,type1,phi)  
-  call state2kirk(n,phi,tip,tiq,rho1,nmax,work1)
+!   print*, "A la entrada de state2kirk, main", n,phi(0),rho1(0,0),nmax,work1(1)
+  call state2kirk(n,phi,0d0,0d0,rho1,nmax,work1)
+!   print*, "En el main  , salida de state2kik", rho1(0,0)
+!   print*,n,rho1(0,0), nmax, nr, wigner(0,0), ndist, work1(1)
   call kirk2wig(1,n,rho1,nmax,nr,wigner,ndist,work1)
+!   print*, "Main", wigner(0,0)
   nx=n*nr ;ny=nx
   wigner=wigner/real(nx,8)
+!   print*, "En la main  , salida de kirk2wig", wigner(0,0)
   call zfft2d(1,nx,nx,wigner,ndist,work1)
   call zfft2d(-1,nx,nx,wigner,ndist,work1)
+!   print*, "En la main  , salida de zfft2d", wigner(0,0), nx**2
   wigner=wigner/(nx**2)
 ! }}}
 ! EScritura {{{
+        write(*,*) real(wigner(0,0),8),wigner_tst(1,1)
+!         write(*,*) sum(abs(wigner-wigner_tst))
+    tmp=0d0
     do i=0,nx-1
       do j=0,nx-1
-        write(*,*) i,j,real(wigner(i,j),8)
+         tmp=tmp+abs(real(wigner(i,j),8)-wigner_tst(i+1,j+1))
+!         write(*,*) i,j,real(wigner(i,j),8),wigner_tst(i,j)
       end do
     end do
+    print*, tmp
   ! }}}
+ deallocate (phi_a, wigner_tst)
  contains
 !! subroutines {{{
+subroutine wigner_from_state_2(phi,wigner) !{{{
+  implicit none
+  complex(kind(1d0)), intent(in)   :: phi(:)
+  complex(kind(1d0)), allocatable  :: wigner_tmp(:,:)
+  real(kind(1d0)), intent(out)  :: wigner(:,:)
+  complex(kind(1d0)), allocatable  :: rho1(:,:), work1(:)
+  integer n, nr
+  n=size(phi)
+  allocate (rho1(n,n), work1(4*n+16), wigner_tmp(2*n,2*n))
+  if ((size(wigner,1).ne.2*n).or.(size(wigner,2).ne.2*n)) then
+    print*,"Tamanos no compatibles en la rutina wigner_from_state"
+    print*,size(phi), size(wigner,1), size(wigner,2)
+  endif
+  wigner_tmp=0d0
+  call state2kirk(n,phi,0d0,0d0,rho1,n,work1)
+  call kirk2wig(1,n,rho1,n,nr,wigner_tmp,2*n,work1)
+  wigner_tmp=wigner_tmp/(2*n)
+  call zfft2d(1,2*n,2*n,wigner_tmp,2*n,work1)
+  call zfft2d(-1,2*n,2*n,wigner_tmp,2*n,work1)
+  wigner_tmp=wigner_tmp/(4*n*n)
+  deallocate(rho1, work1)
+  wigner=real(wigner_tmp)
+  deallocate(wigner_tmp)
+end subroutine wigner_from_state_2 ! }}}
+
 ! }}}
 end  Program semiagam
