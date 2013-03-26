d643c6ccc54c8895ec6be45f83bab447fc63aa85
Carlos Pineda <carlospgmat03@gmail.com>
2013-03-25 18:56:20 -0600

    Corrected the path of cfp_math.h

--- cpp/itpp_ext_math.h
+++ cpp/itpp_ext_math.h
@@ -1,6 +1,6 @@
 #ifndef ITPP_EXT_MATH_H
 #define ITPP_EXT_MATH_H
-// #include <itpp/itbase.h>
+#include <itpp/itbase.h>
 namespace itppextmath{ // {{{ .h
 template <class Num_T>  double test_symmetry(const itpp::Mat<Num_T >);
 double test_real_symmetric(const itpp::cmat);
