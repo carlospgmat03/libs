633dde662d28679a839550a9e3490bb84ba72a03
Carlos Pineda <carlospgmat03@gmail.com>
2014-01-22 17:54:17 -0600

    Se parte la rutina ApplyIsingStar que parecia estar dañana, pues no tenia la interaccion interna. Asi se puede aplicar a un par de hamiltonianos mas

--- SpinChain.m
+++ SpinChain.m
@@ -93,9 +93,27 @@ Begin["Private`"]
   Qubits = Log[2, Length[state]];
 If[IntegerQ[Qubits]==False,Print["Error: The state does not correspond to a integer number of qubits"];Abort[]];
   statenew=state;
-  For[q=1, q<Qubits, q++, 
-  statenew = ApplyIsing[statenew, Jint, 0 , q];
-]];
+  statenew = ApplyIsingStarQubitEnvironment[statenew, Jint]
+  statenew = ApplyIsingStarQubitEnvironment[statenew, Jenv]
+  statenew
+  ];
+(* }}} *)
+(* {{{ *) ApplyIsingStarInternalInteraction[state_?VectorQ, Jenv_, Jint_] := Module[{Qubits, statenew, QubitToAdress, q},
+  Qubits = Log[2, Length[state]];
+If[IntegerQ[Qubits]==False,Print["Error: The state does not correspond to a integer number of qubits"];Abort[]];
+  statenew=state;
+  For[q=1, q<Qubits-1, q++, statenew = ApplyIsing[statenew, Jint, q , q+1]; ];
+  statenew = ApplyIsing[statenew, Jint, Qubits-1 , 1]; 
+  statenew
+  ];
+(* }}} *)
+(* {{{ *) ApplyIsingStarQubitEnvironment[state_?VectorQ, Jint_] := Module[{Qubits, statenew, QubitToAdress, q},
+  Qubits = Log[2, Length[state]];
+If[IntegerQ[Qubits]==False,Print["Error: The state does not correspond to a integer number of qubits"];Abort[]];
+  statenew=state;
+  For[q=1, q<Qubits, q++, statenew = ApplyIsing[statenew, Jint, 0 , q]; ];
+  statenew
+  ];
 (* }}} *)
 (* }}} *)
 (* {{{ Explicit Matrices *)
