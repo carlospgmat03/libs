d51630c75d2bd5a52d64dd9f0eec83aae6437dc6
davidphysdavalos <davidwarro@gmail.com>
2017-03-10 13:59:34 -0600

    se usa miembro del grupo cociente que faltaba, parece que el de total inversion no se necesitara de momento

--- Quantum2.m
+++ Quantum2.m
@@ -482,8 +482,8 @@ c=Transpose[c];
 n=x.g.Transpose[x];
 {eig,o}=Eigensystem[n]//Chop;
 rightL=ForceSameSignatureForLorentz[Transpose[x].Transpose[o]]//Chop;
-If[Det[rightL]==-1,rightL=g.rightL;];
-If[Det[leftL]==-1,leftL=g.leftL;];
+If[Det[rightL]==-1,If[rightL[[1,1]]<0,rightL=(-g).rightL;,rightL=(g).rightL;]];
+If[Det[leftL]==-1,If[leftL[[1,1]]<0,leftL=(-g).leftL;,leftL=(g).leftL;]];
 If[(LorentzMatrixQ[leftL]&&LorentzMatrixQ[rightL])==False,Print["Decomposition not done"];,{leftL,Transpose[leftL].matrix.rightL,rightL}//Chop]
 ];
 
