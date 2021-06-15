ddbcc6b2b8655e569402c11d0b4ccc8e6d9f85c6
davidphysdavalos <davidwarro@gmail.com>
2021-06-14 20:23:10 -0500

    se añade rutina para canal complementario

--- Quantum2.m
+++ Quantum2.m
@@ -103,6 +103,7 @@ KrausQubitParticles::usage = "KrausQubitParticles[channel_,particles_]."
 KrausQubitParticlesFromPauliProductBasis::usage = "KrausQubitParticlesFromPauliProductBasis[channel_?MatrixQ]."
 CPQPauli::usage = "Complete positvity test for Pauli channels, with lambdas as the input, CPQPauli[\[Lambda]_]."
 PositiveAndNegativeParts::usage = "PositiveAndNegativeParts[M_] returns the positve and negative parts of M such that M=M^+-M^- where M^{+-} are posive-semidefinite and positive, respectively."
+ComplementaryChannelFromKrausList::usage = "ComplementaryChannelFromKrausList[list][\[Rho]], computes the complementary channel accordingly Stinespring dilation theorem. Be aware that the complementary channel in general is rectangular, the dimension of the image is the Kraus rank."
 
 
 Begin["Private`"] 
@@ -782,6 +783,11 @@ Select[eig//Chop//Transpose,#[[1]]>= 0&];
 PositiveM=Select[eig//Chop//Transpose,#[[1]]>= 0&];
 NegativeM=Select[eig//Chop//Transpose,#[[1]]< 0&];
 {Sum[PositiveM[[i]][[1]]Proyector[PositiveM[[i]][[2]]],{i,Length[PositiveM]}],Sum[-NegativeM[[i]][[1]]Proyector[NegativeM[[i]][[2]]],{i,Length[NegativeM]}]}
+];
+
+ComplementaryChannelFromKrausList[list_][\[Rho]_]:=Module[{dim},
+dim=Length[list];
+Sum[Tr[list[[i]] . \[Rho] . Dagger[list[[j]]]]Proyector[BasisState[i-1,dim],BasisState[j-1,dim]],{i,dim},{j,dim}]
 ];
 
 End[]
